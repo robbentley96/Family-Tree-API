2ad628c7bd4c34ee2666b2b366f3bfe8db33e16a
Robert Bentley <Robert.Bentley@h-f.co.uk>
2023-10-25 14:27:13 +0100

    net6.0 and sqlite

---
+++ FamilyTreeAPI/Migrations/20231024142942_Initial.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FamilyTreeAPI.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Marriages",
+                columns: table => new
+                {
+                    MarriageID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Person1ID = table.Column<int>(nullable: false),
+                    Person2ID = table.Column<int>(nullable: false),
+                    StartDate = table.Column<DateTime>(nullable: false),
+                    EndDate = table.Column<DateTime>(nullable: false),
+                    StartYear = table.Column<int>(nullable: false),
+                    EndYear = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Marriages", x => x.MarriageID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "People",
+                columns: table => new
+                {
+                    PersonID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstName = table.Column<string>(nullable: true),
+                    MiddleName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    MaidenName = table.Column<string>(nullable: true),
+                    DateOfBirth = table.Column<DateTime>(nullable: true),
+                    DateOfDeath = table.Column<DateTime>(nullable: true),
+                    YearOfBirth = table.Column<int>(nullable: true),
+                    YearOfDeath = table.Column<int>(nullable: true),
+                    Parent1ID = table.Column<int>(nullable: true),
+                    Parent2ID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_People", x => x.PersonID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Marriages");
+
+            migrationBuilder.DropTable(
+                name: "People");
+        }
+    }
+}
--- FamilyTreeAPI/Startup.cs
+++ FamilyTreeAPI/Startup.cs
@@ -1,18 +1,30 @@
 ﻿using System;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-
+using Microsoft.EntityFrameworkCore.Sqlite;
 [assembly: FunctionsStartup(typeof(FamilyTreeAPI.Startup))]
 
 namespace FamilyTreeAPI
 {
     public class Startup : FunctionsStartup
     {
+        public Startup()
+        {
+
+        }
+        public Startup(IHostingEnvironment env)
+        {
+            using(var client = new FamilyTreeContext())
+            {
+                client.Database.EnsureCreated();
+            }
+        }
         public override void Configure(IFunctionsHostBuilder builder)
         {
-            builder.Services.AddDbContext<FamilyTreeContext>(options => options.UseSqlServer(Environment.GetEnvironmentVariable("FamilyTreeDatabase")));
+            builder.Services.AddDbContext<FamilyTreeContext>(options => options.UseSqlite("Data Source=../../../FamilyTree.db"));
             builder.Services.AddTransient<IPersonService, PersonService>();
         }
     }
